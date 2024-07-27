c3c39c1ba4ef79139b661c770d0513e57abb17c4
MarechalBanane <marechalbanane@protonmail.com>
2024-07-28 00:25:18 +0200

    data orga (WIP)

--- midi2pico8/midi2pico8.cpp
+++ midi2pico8/midi2pico8.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <Windows.h>
+#include <map>
 
 #include "../RtMidi.h"
 
@@ -17,23 +18,125 @@ std::vector< unsigned char >* old_message;
 std::vector<std::string> padkeyNames = { "Down", "PgDown", "Left", "Right", "Up", "PgUp", "", "" };
 std::vector<std::string> buttonkeyNames = { "Home", "-", "+", "Del", "Enter", "Space"};
 
-typedef struct s_pad
+typedef struct s_key
 {
-	int cc;
-	char vk;
 	int scs;
+	bool ext;
+	char name[10];
 };
 
-std::vector<s_pad> pads = {
-	{36,VK_DOWN,0x50},
-	{38,VK_NEXT,0x51},
-	{42,VK_LEFT,0x4b},
-	{46,VK_RIGHT,0x4d},
-	{50,VK_UP,0x48},
-	{45,VK_PRIOR,0x49},
-	{51,0,0},
-	{49,0,0},
+std::map<char, s_key> g_keys = {
+	{VK_DOWN,{0x50,true,"Down"}},
+	{VK_NEXT,{0x51,true,"PgDown"}},
+	{VK_LEFT,{0x4b,true,"Left"}},
+	{VK_RIGHT,{0x4d,true,"Right"}},
+	{VK_UP,{0x48,true,"Up"}},
+	{VK_PRIOR,{0x49,true,"PgUp"}},
+	{'Z',{0x2c,false,"Z"}},
+	{'S',{0x1f,false,"S"}},
+	{'X',{0x2d,false,"X"}},
+	{'D',{0x20,false,"D"}},
+	{'C',{0x2e,false,"C"}},
+	{'V',{0x2f,false,"V"}},
+	{'G',{0x22,false,"G"}},
+	{'B',{0x30,false,"B"}},
+	{'H',{0x23,false,"H"}},
+	{'N',{0x31,false,"N"}},
+	{'J',{0x24,false,"J"}},
+	{'M',{0x32,false,"M"}},
+	{'Q',{0x10,false,"Q"}},
+	{'2',{0x3,false,"2"}},
+	{'W',{0x11,false,"W"}},
+	{'3',{0x4,false,"3"}},
+	{'E',{0x12,false,"E"}},
+	{'R',{0x13,false,"R"}},
+	{'5',{0x6,false,"5"}},
+	{'T',{0x14,false,"T"}},
+	{'6',{0x7,false,"6"}},
+	{'Y',{0x15,false,"Y"}},
+	{'7',{0x8,false,"7"}},
+	{'U',{0x16,false,"U"}},
+	{'I',{0x17,false,"I"}},
+	{'9',{0xa,false,"9"}},
+	{'O',{0x18,false,"O"}},
+	{'0',{0xb,false,"0"}},
+	{'P',{0x19,false,"P"}},
 };
+
+std::map<char, char> g_notes = {
+	{48,'Z'},
+	{49,'S'},
+	{50,'X'},
+	{51,'D'},
+	{52,'C'},
+	{53,'V'},
+	{54,'G'},
+	{55,'B'},
+	{56,'H'},
+	{57,'N'},
+	{58,'J'},
+	{59,'M'},
+	{60,'Q'},
+	{61,'2'},
+	{62,'W'},
+	{63,'3'},
+	{64,'E'},
+	{65,'R'},
+	{66,'5'},
+	{67,'T'},
+	{68,'6'},
+	{69,'Y'},
+	{70,'7'},
+	{71,'U'},
+	{72,'I'},
+	{73,'9'},
+	{74,'O'},
+	{75,'0'},
+	{76,'P'},
+};
+
+std::map<char,char> g_pads = {
+	{36,VK_DOWN},
+	{38,VK_NEXT},
+	{42,VK_LEFT},
+	{46,VK_RIGHT},
+	{50,VK_UP},
+	{45,VK_PRIOR},
+	{51,0},
+	{49,0},
+};
+
+bool keyhit(char vk)
+{
+	if (vk != 0)
+	{
+		s_key key = g_keys.at(vk);
+		if (key.scs != 0)
+		{
+			INPUT ip;
+			ip.type = INPUT_KEYBOARD;
+			ip.ki.time = 0;
+			ip.ki.wVk = vk;
+			ip.ki.wScan = key.scs;
+			ip.ki.dwExtraInfo = GetMessageExtraInfo();
+			ip.ki.dwFlags = 0;
+			if (key.ext)
+			{
+				ip.ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
+				ip.ki.wScan |= 0xE000;
+			}
+
+			SendInput(1, &ip, sizeof(INPUT));
+			ip.ki.dwFlags |= KEYEVENTF_KEYUP;
+			SendInput(1, &ip, sizeof(INPUT));
+			std::cout << "key hit " << key.name << "\n";
+
+			return true;
+		}
+	}
+
+	return false;
+}
 
 void mycallback(double deltatime, std::vector< unsigned char > *message, void *userData)
 {
@@ -47,72 +150,31 @@ void mycallback(double deltatime, std::vector< unsigned char > *message, void *u
 	int type = message->at(0);
 	if (type == MESSAGE_TYPE_NOTE)
 	{
-		// 36 -> 84
 		int note = message->at(1);
-		char vks[] = "ZSXDCVGBHNJMQ2W3ER5T6Y7UI9O0P";
-		int scs[] =
+		char vk = g_notes.at(note);
+		if (vk!=0)
 		{
-			0x2c, 0x1f, 0x2d, 0x20, 0x2e, 0x2f, 0x22, 0x30, 0x23, 0x31, 0x24, 0x32,
-			0x10, 0x3, 0x11, 0x4, 0x12, 0x13, 0x6, 0x14, 0x7, 0x15, 0x8, 0x16, 0x17, 0xa, 0x18, 0xb, 0x19
-		};
-		if (note >= 48 && note <= 76)
-		{
-			auto vk = vks[note - 48];
-			auto sc = scs[note - 48];
-			auto hCurrentWindow = GetForegroundWindow();
-
-			INPUT ip;
-			ip.type = INPUT_KEYBOARD;
-			ip.ki.time = 0;
-			ip.ki.wVk = vk;
-			ip.ki.wScan = sc; // note - 48; //VK_RETURN is the code of Return key
-
-			ip.ki.dwExtraInfo = 0;
-			ip.ki.dwFlags = 0;
-			SendInput(1, &ip, sizeof(INPUT));
-			ip.ki.dwFlags = KEYEVENTF_KEYUP;
-			SendInput(1, &ip, sizeof(INPUT));
-
-			std::cout << "note " << note << ", key " << vk << "\n";
-
-			old_message = message;
+			if (!keyhit(vk))
+			{
+				std::cout << "note " << note << "\n";
+			}
 		}
+
+		old_message = message;
 	}
 	else
 	{
-		
 		// only correct for axiom 25 with default settings
 		if (type == MESSAGE_TYPE_PAD)
 		{
 			int pad = message->at(1);
 			if (message->at(2) != 0)
 			{
-				bool found = false;
-				for (int i = 0; i < pads.size(); i++)
+				char vk=g_pads.at(pad);
+				if (!keyhit(vk))
 				{
-					s_pad ipad = pads[i];
-					if (pad == ipad.cc)
-					{
-						if (ipad.vk != 0)
-						{
-							INPUT ip;
-							ip.type = INPUT_KEYBOARD;
-							ip.ki.time = 0;
-							ip.ki.wVk = ipad.vk;
-							ip.ki.wScan = 0xE000 | ipad.scs;
-							ip.ki.dwExtraInfo = GetMessageExtraInfo();
-							ip.ki.dwFlags = KEYEVENTF_EXTENDEDKEY;
-							SendInput(1, &ip, sizeof(INPUT));
-							ip.ki.dwFlags |= KEYEVENTF_KEYUP;
-							SendInput(1, &ip, sizeof(INPUT));
-							found = true;
-							std::cout << "pad " << pad << ", key " << padkeyNames[i] << "\n";
-							break;
-						}
-					}
-				}
-				if (!found)
 					std::cout << "pad " << pad << "\n";
+				}
 			}
 		}
 		else if (type == MESSAGE_TYPE_BTN)
